775e68d8ad1244c426af40487e6ecfb52e06ef09
staslolpol <staspriho9@gmail.com>
2026-04-22 17:15:30 +0300

    Task03 complite

--- Task01/logic.cpp
+++ Task01/logic.cpp
@@ -4,3 +4,29 @@
 // Дана математическая прямоугольная матрица размером N на M.
 // Необходимо разработать функцию (или программу), которая 
 // вычисляет среднее арифметическое ненулевых элементов матрицы.
+#include "util.h"
+
+double calculate_arithmetical_mean_of_nonzero_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n, int m) {
+	if (n < 1 || m < 1) {
+		return -1;
+	}
+
+	double sum = 0;
+	int count = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+
+		for (int j = 0; j < m; j++)
+		{
+			if (matrix[i][j] != 0) {
+				sum += matrix[i][j];
+				count++;
+			}
+		}
+	}
+
+	count <= 0 ? count++ : count;
+
+	return sum / count;
+}
--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -5,3 +5,37 @@
 // Необходимо разработать функцию (или программу), которая находит
 // количество строк, в которых положительных элементов больше 
 // чем всех остальных (отрицательных и нулевых).
+#include "util.h"
+
+int count_rows_with_more_positive_values(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n, int m) {
+	int counter_position = 0;
+	int counter_zero_negative = 0;
+	int counter = 0;
+
+	if (n <= 0 || m <= 0) {
+		return -1;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		counter_position = 0;
+		counter_zero_negative = 0;
+
+		for (int j = 0; j < m; j++)
+		{
+			if (matrix[i][j] <= 0) {
+				counter_zero_negative += 1;
+			}
+			else {
+				counter_position += 1;
+			}
+		}
+
+		if (counter_zero_negative < counter_position) {
+			counter++;
+		}
+		
+	}
+
+	return counter;
+}
