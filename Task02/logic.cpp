47c7159ceff81cf7c1950d05ec3882150fef4bed
staslolpol <staspriho9@gmail.com>
2026-04-22 17:17:41 +0300

    Task02 complite

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -5,3 +5,41 @@
 // Необходимо разработать функцию (или программу), 
 // которая вычисляет сумму элементов матрицы, 
 // расположенных на главной и побочной диагоналях.
+#include "util.h"
+
+
+int sum_main_and_second_diagonales_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n) {
+    if (n < 1) {
+        return -1;
+    }
+
+    int sum = sum_main_elements(matrix, n);
+    sum += sum_additional_elements_without_intersection(matrix, n);
+
+    return sum;
+}
+
+int sum_main_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n) {
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += matrix[i][i];
+    }
+
+    return sum;
+}
+
+int sum_additional_elements_without_intersection(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n) {
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i != n - 1 - i)
+        {
+            sum += matrix[i][n - 1 - i];
+        }
+    }
+
+    return sum;
+}
--- Task02/logic.h
+++ Task02/logic.h
@@ -1,3 +1,7 @@
 ﻿#define DEFAULT_SIZE 100
 
 int sum_main_and_second_diagonales_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n);
+
+int sum_main_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n);
+
+int sum_additional_elements_without_intersection(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n);
